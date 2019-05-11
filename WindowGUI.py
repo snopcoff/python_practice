9bc6fbecff9aee6ad7ddad43352c48a575f1ba09
snopcoff <snoopy.8994@gmail.com>
2019-05-11 19:23:04 +0900

    Window background setting

--- WindowGUI.py
+++ WindowGUI.py
@@ -7,5 +7,12 @@ window = tkinter.Tk()
 window.title( "Lili's question" )
 window.minsize( 640, 480 )
 
+# Background setting
+canvas = tkinter.Canvas( bg = "pink", width = 640, height = 480 )
+canvas.place( x = 0, y = 0 )
+
+img = tkinter.PhotoImage( file = "./img3/chap3-back.png" )
+canvas.create_image( img.width()/2, img.height()/2, image = img )
+
 # Main loop
 window.mainloop()
